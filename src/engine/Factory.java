303e0f5d3c905b61ca2c88b797abdeec6005656f
Sebastian Oleszczuk <sebastian.oleszczuk@bunge.com>
2018-07-11 12:56:08 +0200

    factory test

---
+++ src/engine/Factory.java
@@ -0,0 +1,22 @@
+package engine;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Factory {
+	
+	ConcurrentHashMap<String, V> mapa;
+	
+	public Factory getCountry(String country) {
+		
+		
+		
+		
+		return null;
+	}
+
+	
+	
+	
+}	
+
+
